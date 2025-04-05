6ab12cad7f9daa85717720658fe4e14249159dba
RalphCajazeira <ralphmtk@gmail.com>
2025-04-05 15:00:30 -0300

    ajustado knex para testar produção, teste local ok

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -1,25 +1,20 @@
 require("dotenv").config();
 const { parse } = require("pg-connection-string");
 
-// Transforma a DATABASE_URL em objeto
 const dbConfig = parse(process.env.DATABASE_URL);
 
 module.exports = {
-  development: {
-    client: "pg",
-    connection: {
-      host: dbConfig.host,
-      port: dbConfig.port,
-      database: dbConfig.database,
-      user: dbConfig.user,
-      password: dbConfig.password,
-      ssl: {
-        rejectUnauthorized: false,
-      },
-    },
-    migrations: {
-      directory: "./database/migrations",
-    },
-    useNullAsDefault: true,
+  client: "pg",
+  connection: {
+    host: dbConfig.host,
+    port: dbConfig.port,
+    database: dbConfig.database,
+    user: dbConfig.user,
+    password: dbConfig.password,
+    ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
   },
+  migrations: {
+    directory: "./database/migrations",
+  },
+  useNullAsDefault: true,
 };
